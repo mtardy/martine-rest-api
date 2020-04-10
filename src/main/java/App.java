409b655b7521a4ac5a893968d44de0b98463f3fa
mtardy <mahe5397@hotmail.fr>
2020-04-10 12:55:23 +0200

    Enable CORS for all origins

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import api.Ping;
 import api.RecetteAPI;
 import io.swagger.jaxrs.config.BeanConfig;
+import org.jboss.resteasy.plugins.interceptors.CorsFilter;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
@@ -10,6 +11,8 @@ import java.util.Set;
 @ApplicationPath("/v1")
 public class App extends Application {
 
+    private Set<Object> singletons = new HashSet<Object>();
+
     public App(){
         BeanConfig beanConfig = new BeanConfig();
         beanConfig.setVersion("1.0.0");
@@ -17,6 +20,16 @@ public class App extends Application {
         beanConfig.setTitle("Les recettes de Martine API");
         beanConfig.setDescription("Bienvenue sur l'API REST des recettes de Martine!");
         beanConfig.setScan(true);
+
+        CorsFilter corsFilter = new CorsFilter();
+        corsFilter.getAllowedOrigins().add("*");
+        corsFilter.setAllowedMethods("OPTIONS, GET, POST, DELETE, PUT, PATCH");
+        singletons.add(corsFilter);
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
     }
 
     @Override
