8a342a3d1b95c09dfda73b9d939340dd6b9e89cc
mtardy <mahe5397@hotmail.fr>
2020-04-26 12:52:04 +0200

    Modify RecetteCompact to include more information

--- src/main/java/models/format/RecetteCompact.java
+++ src/main/java/models/format/RecetteCompact.java
@@ -1,14 +1,25 @@
 package models.format;
 
 import models.Recette;
+import java.time.LocalDateTime;
 
 public class RecetteCompact {
     private int id;
     private String nom;
+    private String auteurUsername;
+    private LocalDateTime dateCreation;
+    private LocalDateTime dateModification;
+    private String description;
+    private String photo;
 
     public RecetteCompact(Recette r) {
         this.id = r.getId();
         this.nom = r.getNom();
+        this.auteurUsername = r.getAuteurUsername();
+        this.dateCreation = r.getDateCreation();
+        this.dateModification = r.getDateModification();
+        this.description = r.getDescription();
+        this.photo = r.getPhoto();
     }
 
     public int getId() {
@@ -25,5 +36,45 @@ public class RecetteCompact {
 
     public void setNom(String nom) {
         this.nom = nom;
+    }
+
+    public String getAuteurUsername() {
+        return auteurUsername;
+    }
+
+    public void setAuteurUsername(String auteurUsername) {
+        this.auteurUsername = auteurUsername;
+    }
+
+    public LocalDateTime getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(LocalDateTime dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public LocalDateTime getDateModification() {
+        return dateModification;
+    }
+
+    public void setDateModification(LocalDateTime dateModification) {
+        this.dateModification = dateModification;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
     }
 }
