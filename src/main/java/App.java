a01b354c258c3aff82271940c7d56723a4ca9b23
mtardy <mahe5397@hotmail.fr>
2020-04-30 03:12:26 +0200

    Version bump to 1.2.1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -20,7 +20,7 @@ public class App extends Application {
 
     public App(){
         BeanConfig beanConfig = new BeanConfig();
-        beanConfig.setVersion("1.2.0");
+        beanConfig.setVersion("1.2.1");
         beanConfig.setBasePath("/api/v1");
         beanConfig.setTitle("Les recettes de Martine API");
         beanConfig.setDescription("Bienvenue sur l'API REST des recettes de Martine!");
